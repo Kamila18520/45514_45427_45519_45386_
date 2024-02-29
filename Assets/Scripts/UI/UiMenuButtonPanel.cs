e2e81ca3210a47878236093c9ce39cc7eed18282
Kamila <kamila.mspychala@vp.pl>
2024-02-29 18:28:58 +0100

    credits

--- Assets/Scripts/UI/UiMenuButtonPanel.cs
+++ Assets/Scripts/UI/UiMenuButtonPanel.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class UiMenuButtonPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] RectTransform[] buttonRectTransform;
+    [SerializeField] RectTransform[] panelRectTransform;
+    [SerializeField] Vector2[] panelVector2;
+    [SerializeField] Vector2[] buttonVector2;
+
+    float duration = 5f;
+
+
+
+    private void Awake()
     {
-        
+        buttonVector2 = new Vector2[buttonRectTransform.Length];
+        panelVector2 = new Vector2[panelRectTransform.Length];
+
+        for (int i = 0; i < buttonRectTransform.Length; i++)
+        {
+            buttonVector2[i] = buttonRectTransform[i].sizeDelta;
+        }
+
+        for (int i = 0; i < panelRectTransform.Length; i++)
+        {
+            panelVector2[i] = panelRectTransform[i].sizeDelta;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    enum State { On, Off }
+    State currentState = State.Off;
+
+    public void CreditsButtons(int i)
     {
-        
+
+        SetRectParameters(i);
     }
+
+    public void OptionsButton(int i)
+    {
+
+        SetRectParameters(i);
+    }
+
+    void SetRectParameters(int i)
+    {
+        int number = i;
+        if (currentState == State.On)
+        {
+
+            currentState = State.Off;
+            AnimatePanel(panelVector2[i], buttonVector2[i], number);
+
+        }
+        else if (currentState == State.Off)
+        {
+            currentState = State.On;
+            AnimatePanel(buttonVector2[i], panelVector2[i], number);
+        }
+
+    }
+
+    private void AnimatePanel(Vector2 startVector2, Vector2 targetVector2, int number)
+    {
+        buttonRectTransform[number].sizeDelta = targetVector2;
+    }
+
+
 }
+
